6f256149415fe4978bf9c76d00a00ca0d08bddbf
rodrigosy3 <sihues3@gmail.com>
2023-08-07 16:25:05 -0500

    Commit 1: Creación del proyecto web Gonrod

---
+++ src/main/java/com/project/appwebgonrod/AppWebGonrodApplication.java
@@ -0,0 +1,13 @@
+package com.project.appwebgonrod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebGonrodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebGonrodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/appwebgonrod/AppWebGonrodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.appwebgonrod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebGonrodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
